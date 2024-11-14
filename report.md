0619e0e3e3a3a00c45cfb5247373af298ae09677
Mia3105 <miarettoc@gmail.com>
2024-11-14 09:02:08 +0100

    Ajouter les fichiers

--- report.md
+++ report.md
@@ -1,17 +1,45 @@
-Journal de bord
-----------------------------------------------------------
-Semaine 1
-Tache       | Temps estimé  |  Temps passé  |     Commentaires
- Vue        |  15 minutes   |   10 minutes  |      
- Bootstrap  |  30 minutes   |               |
- Quiz       |  30 minutes   |               |
- Rapport    |  20 minutes   |               |
+## Journal de bord
 
+Semaine 1
+Tache | Temps estimé | Temps passé | Commentaires
+Vue | 15 minutes | 10 minutes |  
+Bootstrap | 30 minutes | 20 minutes |
+Quiz | 1h | 10h45 - 13h + ... |
+Rapport | 20 minutes | |
 
 Difficultés :
-
+Dans un premier temps, j'ai trouvé assez difficile de faire le compteur de score et de faire le bouton Réinitialiser. J'ai encore un problème, car si toutes les questions sont complétées, le bouton pour réinitialiser fonctionne une première fois comme le bouton Temriner et ensuite en appuyant une deuxième fois, il réinitialise.
 
 Explications et réflexions sur le code :
+Questions :
 
+    1. Expliquer le rôle des fichiers suivants :
+    - main.ts
+        Il permet d'appeler les modules dont nous aurons besoin dans tous les autres fichiers.
+    - main.css
+        Il permet de définir le style de la page du quiz avec du css.
+    - App.vue
+        Il permet de générer un lien URL en local pour visualiser la page.
+    - router/index.ts
+        C'est un fichier TypeScript. Il permet de ...
+    - AboutView.vue
+        Il permet de définir la mise en page du quiz.
+    - HomeView.vue
+
+    - QuizForm.vue
+        Il permet d'écrire tout ce qui sera afficher dans le quiz.
+
+
+    2. Dans le fichier QuizForm.vue :
+    - Quelles sont les similarités et les différences entre ref et computed ?
+        Les deux permettent de définir un objet. ref permet de définir un string, int, ... et le computed permet de définir une fonction, quelque chose qui va être lu et executé.
+    - Que se passe-t-il lorsqu'on clique sur le bouton "Terminer" ?
+        Lorsque l'on clique sur le bouton Terminer, on rentre dans la fonction submit. Cette fonction va compter le nombre de point qui ont été fait et va ensuite ouvrir une fenêtre textuelle qui va afficher un récapitulatif dans réponses données et qui va ensuite afficher le score.
+    - Qu'est-ce qu'un v-model ?
+        Cela permet de faire le lien entre la page et le code. S'il y a un changement dans le code, cela changera sur la page et inversément.
+
+    - À quoi sert le :class="{ disabled: !filled }" ?
+        Cela désactive le bouton Terminer tant que la fonction filled ne revoie pas vrai, c'est-à-dire tant que toutes les question n'ont pas de réponses. Il faut répondre à toutes les questions pour pouvoir appuyer sur le bouton Terminer.
 
 Suite du projet :
+Je trouve frustrant de devoir à chaque fois tout réécrire, autant pour créer les questions que dans la fonction filled. Il faudrait trouver un moyen d'écrire cela de façon plus concise.
--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -1,15 +1,48 @@
 <script setup lang="ts">
-  import { computed, ref } from "vue";
+import { computed, ref } from 'vue'
 
-  const cheval = ref<string | null>(null);
-  const filled = computed<boolean>(() => cheval.value !== null);
+const cheval = ref<string | null>(null)
+const chat = ref<string | null>(null)
+const capitale = ref<string | null>(null)
+const filled = computed<boolean>(
+  () => cheval.value !== null && chat.value !== null && capitale.value !== null,
+)
 
-  function submit(event: Event): void {
-    event.preventDefault();
-    if (filled.value) {
-      alert(`Vous avez choisi la couleur ${cheval.value} !`);
-    }
+function submit(event: Event): void {
+  event.preventDefault()
+
+  let point = 0
+  if (cheval.value == 'blanc') {
+    point = point + 1
   }
+  if (chat.value == '4 pattes') {
+    point = point + 1
+  }
+  if (capitale.value == 'Berne') {
+    point = point + 1
+  }
+
+  let message: string = ''
+  if (point == 3) {
+    message = "Bravo ! C'est parfait !"
+  }
+
+  if (filled.value) {
+    alert(`
+    Vous avez choisi la couleur ${cheval.value}
+    Vous avez choisi ${chat.value}
+    Vous avez choisi la ville de ${capitale.value}
+
+
+    Vous avez fait ${point} sur 3 !
+    ${message}
+    `)
+  }
+}
+
+function refresh(): void {
+  location.reload()
+}
 </script>
 
 <template>
@@ -48,12 +81,121 @@
       />
       <label class="form-check-label" for="chevalNoir">Noir</label>
     </div>
-    <button
-      class="btn btn-primary"
-      :class="{ disabled: !filled }"
-      type="submit"
-    >
-      Terminer
-    </button>
+    <div class="form-check">
+      <input
+        id="chevalRouge"
+        v-model="cheval"
+        class="form-check-input"
+        type="radio"
+        name="cheval"
+        value="rouge"
+      />
+      <label class="form-check-label" for="chevalRouge">Rouge</label>
+    </div>
+  </form>
+  <br />
+
+  <form @submit="submit">
+    Combien de pattes a un chat ?
+    <div class="form-check">
+      <input
+        id="2pattes"
+        v-model="chat"
+        class="form-check-input"
+        type="radio"
+        name="chat"
+        value="2 pattes"
+      />
+      <label class="form-check-label" for="2pattes">2 pattes</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="4pattes"
+        v-model="chat"
+        class="form-check-input"
+        type="radio"
+        name="chat"
+        value="4 pattes"
+      />
+      <label class="form-check-label" for="4pattes">4 pattes</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="8pattes"
+        v-model="chat"
+        class="form-check-input"
+        type="radio"
+        name="chat"
+        value="8 pattes"
+      />
+      <label class="form-check-label" for="8pattes">8 pattes</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="10pattes"
+        v-model="chat"
+        class="form-check-input"
+        type="radio"
+        name="chat"
+        value="10 pattes"
+      />
+      <label class="form-check-label" for="10pattes">10 pattes</label>
+    </div>
+  </form>
+  <br />
+
+  <form @submit="submit">
+    Quelle est la capitale de la Suisse ?
+    <div class="form-check">
+      <input
+        id="Geneve"
+        v-model="capitale"
+        class="form-check-input"
+        type="radio"
+        name="capitale"
+        value="Genève"
+      />
+      <label class="form-check-label" for="geneve">Genève</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="Lausanne"
+        v-model="capitale"
+        class="form-check-input"
+        type="radio"
+        name="capitale"
+        value="Lausanne"
+      />
+      <label class="form-check-label" for="lausanne">Lausanne</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="Berne"
+        v-model="capitale"
+        class="form-check-input"
+        type="radio"
+        name="capitale"
+        value="Berne"
+      />
+      <label class="form-check-label" for="berne">Berne</label>
+    </div>
+    <div class="form-check">
+      <input
+        id="Zurich"
+        v-model="capitale"
+        class="form-check-input"
+        type="radio"
+        name="capitale"
+        value="Zurich"
+      />
+      <label class="form-check-label" for="zurich">Zurich</label>
+    </div>
+  </form>
+  <br />
+
+  <form @submit="submit">
+    <button class="btn btn-primary" :class="{ disabled: !filled }" type="submit">Terminer</button>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <button class="btn btn-primary" button @:click="refresh">Réinitialiser</button>
   </form>
 </template>
