97e6c082d59301dbf55c36721b4dd7534ab87afb
Mia3105 <miarettoc@gmail.com>
2024-12-12 12:31:03 +0100

    Ajouter les fichiers

--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -26,24 +26,28 @@ const totalScore = computed<number>(() => questionStates.value.length)
 function submit(event: Event): void {
   event.preventDefault()
   questionStates.value = questionStates.value.map(() => QuestionState.Submit)
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function reset(event: Event): void {
   event.preventDefault()
   questionStates.value = questionStates.value.map(() => QuestionState.Vide)
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
+
+
 </script>
 
 <template>
   <form @submit="submit">
-    <div class="container">
-      <div class="row">
-        <div class="col">
+    <br />
           <QuestionRadio
             id="pi"
             v-model="questionStates[0]"
             answer="3.1415926535"
-            text="Quelles sont les 10 premières décimales de π ?"
+            text="1. Quelles sont les 10 premières décimales de π ?"
             :options="[
               { value: '3.1514131211', text: '3.1514131211' },
               { value: '3.1415926535', text: '3.1415926535' },
@@ -59,62 +63,71 @@ function reset(event: Event): void {
             id="planète"
             v-model="questionStates[1]"
             answer="8"
-            text="Combien y a-t-il de planètes dans le système solaire ?"
+            text="2. Combien y a-t-il de planètes dans le système solaire ?"
             placeholder="Veuillez saisir un nombre"
-            answer-detail="Les 8 planètes sont Mercure, Venus, Terre, Mars, Jupiter, Saturne, Uranus, Neptune. Pluton n'est plus considéré comme une planète."
+            answer-detail="Les 8 planètes sont Mercure, Venus, Terre, Mars, Jupiter, Saturne, Uranus, Neptune. Pluton n'est plus considérée comme une planète."
           />
 
           <br />
 
-          <QuestionRadio
-            id="theoreme"
-            v-model="questionStates[2]"
-            answer="Théorème de Pythagore"
-            text="Quel théorème est utilisé uniquement dans les triangles rectangles ?"
-            :options="[
-              { value: 'Théorème de Thalès', text: 'Théorème de Thalès' },
-              { value: 'Théorème des deux gendarmes', text: 'Théorème des deux gendarmes' },
-              { value: 'Théorème de Pythagore', text: 'Théorème de Pythagore' },
-              { value: 'Théorème de Cauchy', text: 'Théorème de Cauchy' },
-            ]"
-            answer-detail="Le théorème de Pythagore permet de faire un lien entre les angles et les longueurs dans un triangle rectangle."
-          />
+            <QuestionRadio
+              id="theoreme"
+              v-model="questionStates[2]"
+              answer="Théorème de Pythagore"
+              text="3. Quel théorème est utilisé uniquement dans les triangles rectangles ?"
+              :options="[
+                { value: 'Théorème de Thalès', text: 'Théorème de Thalès' },
+                { value: 'Théorème des deux gendarmes', text: 'Théorème des deux gendarmes' },
+                { value: 'Théorème de Pythagore', text: 'Théorème de Pythagore' },
+                { value: 'Théorème de Cauchy', text: 'Théorème de Cauchy' },
+              ]"
+              answer-detail="Le théorème de Pythagore permet de faire un lien entre les angles et les longueurs dans un triangle rectangle."
+            />
 
-          <br />
+            <br />
 
-          <QuestionCheckbox
-            id="lausanne"
-            v-model="questionStates[3]"
-            text="Où se situe Lausanne ?"
-            :answer="['Canton de Vaud', 'en Suisse']"
-            :options="[
-              { value: 'Canton de Vaud', text: 'Canton de Vaud' },
-              { value: 'en Suisse', text: 'en Suisse' },
-              { value: 'Canton du Valais', text: 'Canton du Valais' },
-              { value: 'en France', text: 'en France' },
-            ]"
-          />
+            <QuestionCheckbox
+              id="lausanne"
+              v-model="questionStates[3]"
+              text="4. Où se situe Lausanne ?"
+              :answer="['Canton de Vaud', 'en Suisse']"
+              :options="[
+                { value: 'Canton de Vaud', text: 'Canton de Vaud' },
+                { value: 'en Suisse', text: 'en Suisse' },
+                { value: 'Canton du Valais', text: 'Canton du Valais' },
+                { value: 'en France', text: 'en France' },
+              ]"
+            />
+            <br /><br />
+        <p
+          style="
+            position: fixed;
+            top: 85px;
+            left: 0;
+            width: 100%;
+            background: none;
+            margin: 0;
+            text-align: right;
+            padding: 10px;
+          "
+        >
+          États des questions :
+          <br />{{ questionStates }}
+        </p>
 
-          <br />
-          <br />
+        <div v-if="submitted">Score : {{ score }} / {{ totalScore }}</div>
+        <br />
+        <br />
+        <div style="text-align: left">
           <button class="btn btn-primary" :class="{ disabled: !filled }" type="submit">
             Terminer
           </button>
         </div>
 
-        <div class="col">
-          <div> États des questions : {{ questionStates }}</div>
-          <div v-if="submitted">Score : {{ score }} / {{ totalScore }}</div>
-          <br />
-          <br />
-
-          <br />
-          <br />
-          <div style="text-align: right">
-            <button class="btn btn-primary" button @:click="reset">Réinitialiser</button>
-          </div>
-        </div>
+      <div style="text-align: right">
+        <button class="btn btn-primary" button @:click="reset">Réinitialiser</button>
       </div>
-    </div>
+      <br />
+
   </form>
 </template>
